c6bc4439768c16cf79cccf1e746f0cc8abe59bdf
dpointeck <daniel.pointecker@gmail.com>
2023-06-27 21:40:10 +0200

    Switch template to svelte and pnpm

---
+++ apps/web/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Input } from 'ui';
+
+  let name = '';
+</script>
+
+<main class="">
+  <label for="name">Name</label>
+  <Input bind:value={name} />
+  { name }
+</main>
+
---
+++ packages/ui/index.ts
@@ -0,0 +1,2 @@
+export { default as Input } from './lib/Input.svelte';
+export { default as Textarea } from './lib/Textarea.svelte';
